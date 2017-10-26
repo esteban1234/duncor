8e33525547399c75e42f09333ac88f0cec4e3cf3
esteban1234 <tva_libra@hotmail.com>
2017-10-26 17:13:05 -0500

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="Ij2qC70ck2brY39vyjduPENjmjirMs8NOt89LotMzr0" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>DUNCOR CONSTRUCCIONES Y SERVICIOS DE INGENIERIA</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
